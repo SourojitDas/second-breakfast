b98bf61c5538612d29735c89a0e1de4885761f24
Rajesh burla <raaz.burla@gmail.com>
2019-03-20 18:39:13 +0000

    handled exceptions

--- recipeModelling.py
+++ recipeModelling.py
@@ -9,9 +9,12 @@ user_model_collection = db["recipe-models"]
 destdir = 'directory of dataset'
 
 files = [f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f))]
-print(files)
+
 for f in files:
-    user = open(destdir+f, "r")
-    print(user)
-    user_loaded_data = json.loads(user.read(), encoding='UTF8')
-    user_model_collection.insert_one(user_loaded_data)
+    try:
+        user = open(destdir+f, "r")
+        user_loaded_data = json.loads(user.read(), encoding='UTF8')
+        user_model_collection.insert_one(user_loaded_data)
+    except:
+        print(f)
+        
