c30e0ed4937edbdbac3609de184da19677a0030a
abhishek <abhishek3.aj@gmail.com>
2019-04-09 19:55:27 +0100

    MOngo integration with server

--- flask/flaskSet.py
+++ flask/flaskSet.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 @app.route("/second-breakfast/show-recipe/")
 def show_recipe():
     return mongo_db_client.get_data_to_set_pref()
+
 @app.route("/second-breakfast/save_user_model/", methods=['POST'])
 def save_new_user_model():
     req_data = request.get_json()
     return mongo_db_client.save_user_model(req_data)
+
+@app.route("/second-breakfast/activity", methods['POST'])
+def submit_activity():
+    req_data = request.get_json()
+    return mongo_db_client.track_activity(req_data)
 
 
 if __name__ == '__main__':
--- flask/mongo_db_client.py
+++ flask/mongo_db_client.py
@@ -2,13 +2,22 @@ import json
 from bson import json_util
 import pymongo
 import os
+username = "AA"
+password = "hayabusa"
+connection = pymongo.MongoClient("vm-18-203-247-130.rosettavm.com:27017",
+                     username='AA',
+                     password='hayabusa',
+                     authSource='admin',
+                     authMechanism='SCRAM-SHA-256')
 
-connection = pymongo.MongoClient("mongodb://localhost:27017")
-db = connection["second-breakfast"]
+db = connection["sophie_dev_1"]
 user_collection = db["users"]
 user_models_collection = db["user-models"]
 business_collection = db['business']
-recipe_collection = db['recipe']
+recipe_collection = db['recipes']
+# user_models_collection = db["user-models"]
+
+
 def save_user_details(user, user_model):
     user_collection.insert(user)
     user_models_collection.insert(user_model)
@@ -36,16 +45,43 @@ def get_data_to_set_pref():
     #data = recipie_data_collection.find({'attributes.cuisine': [sa]})
     for cuisine in cuisine_list:
 
-        data = list(recipe_collection.find({'attributes.cuisine':cuisine}))
-        # print(data.count())
+        data = list(recipe_collection.aggregate([
+            {"$match": {"attributes.cuisine": cuisine}},
+            {"$sample": {"size": 10}}
+        ]))
+
         for elem in data:
+            # print(elem)
             temp = {}
-            # cur_li = []
-            temp["_id"] = elem["_id"]
-            temp["img"] = elem["images"]
+            temp["cuisine"] = elem["attributes"]["cuisine"][0]
+            temp["_id"] = str(elem["_id"])
+            temp["img"] = elem["images"][0]["hostedLargeUrl"]
             temp["name"] = elem["name"]
-            # cur_li.append(temp)
             li.append(temp)
     result = json.dumps(li)
     # print(data)
     return result
+
+
+def get_user_model(userid):
+    return user_models_collection.find_one({"user_id": userid})
+
+def get_recommendation(user_id):
+    user_model = get_user_model(user_id)
+    user_fav_cuisine = user_model["favourite_cuisine"]
+    recommendations = []
+    for cuisine, value in user_fav_cuisine.items():
+        recommendations.append(recipe_collection.aggregate({"attributes.cuisine":cuisine}, {"$sample":{"size": value}}))
+
+def track_activity():
+    return "ok"
+# action = {
+#     image data
+# }
+# def process_feedback(action):
+
+
+
+
+
+
