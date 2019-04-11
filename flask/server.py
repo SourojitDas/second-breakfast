17fbbdf3e8aba6e33cf0c79ce55c6cf7c61d8155
akashdeep-singh <dj.akashds@gmail.com>
2019-04-11 17:22:54 +0100

    json response

--- flask/server.py
+++ flask/server.py
@@ -9,32 +9,49 @@ CORS(app)
 
 @app.route("/second-breakfast/show-recipe/")
 def show_recipe():
-    return mongo_db_client.get_data_to_set_pref()
+    return app.response_class(
+        response=mongo_db_client.get_data_to_set_pref(),
+        mimetype='application/json'
+    )
 
 @app.route("/second-breakfast/save_user_model/", methods=['POST'])
 def save_new_user_model():
     req_data = request.get_json()
-    return mongo_db_client.save_user_model(req_data)
+    return app.response_class(
+        response=mongo_db_client.save_user_model(req_data),
+        mimetype='application/json'
+    )
 
 @app.route("/second-breakfast/activity", methods=['POST'])
 def submit_activity():
     req_data = request.get_json()
-    return mongo_db_client.track_activity(req_data)
+    return app.response_class(
+        response=mongo_db_client.track_activity(req_data),
+        mimetype='application/json'
+    )
 
-@app.route("/second-breakfast/recommendation", methods=['POST'])
-def recommend():
-    req_data = request.get_json()
-    return mongo_db_client.get_recommendation(req_data["user_id"])
+@app.route("/second-breakfast/recommendation/<uid>")
+def recommend(uid):
+    return app.response_class(
+        response=mongo_db_client.get_recommendation(uid),
+        mimetype='application/json'
+    )
 
 @app.route("/second-breakfast/test/")
 def test():
-    return mongo_db_client.get_recommendation("uid01")
+    return app.response_class(
+        response=mongo_db_client.get_recommendation("uid01"),
+        mimetype='application/json'
+    )
 
 @app.route("/second-breakfast/statsitics/", methods=['POST'])
 def statsitics():
     req_data = request.get_json()
     print(req_data)
-    return mongo_db_client.get_dashboard(req_data['user_id'])
+    return app.response_class(
+        response=mongo_db_client.get_dashboard(req_data['user_id']),
+        mimetype='application/json'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
